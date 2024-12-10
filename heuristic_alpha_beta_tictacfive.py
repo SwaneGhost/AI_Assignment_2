d8f837f7ed8b80d3f2c917e90354a8d8906a4783
lea shmilovich <leashmilovich@gmail.com>
2024-12-10 22:30:29 +0200

    implemented alpha-beta-pruning changes - complete heuristic_alpha_beta_tictacfive.py in progress yet to be completed - confused about if the h is passed as a paramater heuristic.py tomorrow

--- heuristic_alpha_beta_tictacfive.py
+++ heuristic_alpha_beta_tictacfive.py
@@ -6,17 +6,17 @@ def alphabeta_max_h(current_game, _heuristic, depth=3):
     global h
     h = _heuristic
     # add code here
-    pass
+    return maximin(current_game, -math.inf, math.inf, depth)
 
 
 def alphabeta_min_h(current_game, _heuristic, depth=3):
     global h
     h = _heuristic
     # add code here
-    pass
+    return maximin(current_game, -math.inf, math.inf, depth)
 
 
-def maximin(current_game, depth):
+def maximin(current_game, depth, alpha, beta):
     global h
     if current_game.is_terminal():
         return current_game.get_score(), None
@@ -25,14 +25,17 @@ def maximin(current_game, depth):
     v = -math.inf
     moves = current_game.get_moves()
     for move in moves:
-        mx, next_move = minimax(move, depth - 1)
+        mx, next_move = minimax(move, depth - 1, alpha, beta)
         if v < mx:
             v = mx
             best_move = move
+            alpha = max(alpha, v)
+            if alpha >= beta:
+                break
     return v, best_move
 
 
-def minimax(current_game, depth):
+def minimax(current_game, depth, alpha, beta):
     global h
     if current_game.is_terminal():
         return current_game.get_score(), None
@@ -41,9 +44,12 @@ def minimax(current_game, depth):
     v = math.inf
     moves = current_game.get_moves()
     for move in moves:
-        mx, next_move = maximin(move, depth - 1)
+        mx, next_move = maximin(move, depth - 1, alpha, beta)
         if v > mx:
             v = mx
             best_move = move
+            beta = min(beta, v)
+            if alpha >= beta:
+                break
 
     return v, best_move
