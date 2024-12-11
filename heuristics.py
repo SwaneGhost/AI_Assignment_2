a7735f6c41809fbca025234afa88f88afccd7e2a
lea shmilovich <leashmilovich@gmail.com>
2024-12-11 21:48:16 +0200

    base heuristic improved but there is a small hidden fault

--- heuristics.py
+++ heuristics.py
@@ -1,12 +1,12 @@
 import numpy as np
 
+
 def base_heuristic(curr_state):
     grid = curr_state.get_grid()
     row, col = len(grid), len(grid[0])
     score_player_1 = calculate_score(grid, row, col, 1)
     score_player_2 = calculate_score(grid, row, col, 2)
     return score_player_1 - score_player_2
-
 
 
 def count_sequences(line, player):
@@ -22,23 +22,15 @@ def count_sequences(line, player):
     """
     count_3 = 0
     count_4 = 0
+
     if len(line) > 4:
         for i in range(len(line) - 3):
-            if i + 4 < len(line) and np.array_equal(line[i:i + 4], [player] * 4):
-                if (i != 0 and line[i - 1] == 0) or line[i + 4] == 0:
+            if i + 4 <= len(line) and np.array_equal(line[i:i + 4], [player] * 4):
+                if (i != 0 and line[i - 1] == 0) or (i + 4 < len(line) and line[i + 4] == 0):
                     count_4 += 1
             elif np.array_equal(line[i:i + 3], [player] * 3):
-                if (i != 0 and line[i - 1] == 0) and (i + 3 < len(line) and line[i + 3] == 0):
+                if i != 0 and line[i - 1] == 0 and i + 3 < len(line) and line[i + 3] == 0:
                     count_3 += 1
-
-                """
-                if i == 0 and i + 4 < len(line) and line[i + 4] == 0 and line[i + 3] == 0:
-                    count_3 += 1
-                elif i - 2 >= 0 and line[i - 1] == 0 and line[i - 2] == 0:
-                    count_3 += 1
-                elif i != 0 and line[i - 1] == 0 and i + 3 < len(line) and line[i + 3] == 0:
-                    count_3 += 1
-                    """
 
     return count_3 + count_4
 
@@ -54,6 +46,7 @@ def calculate_score(grid, row, col, player):
    :param player: Integer representing the player (e.g., 1 or 2).
    :return: Integer score for the given player.
    """
+
     score = 0
     for r in range(row):
         score += count_sequences(grid[r], player)
@@ -63,8 +56,10 @@ def calculate_score(grid, row, col, player):
 
     for d in range(-row + 1, col):
         diag1 = [grid[r][r - d] for r in range(max(0, d), min(row, col + d))]
-        diag2 = [grid[r][d + r] for r in range(max(0, -d), min(row, col - d))]
         score += count_sequences(diag1, player)
+
+    for d in range(col + row - 1):
+        diag2 = [grid[r][d - r] for r in range(max(0, d - col + 1), min(row, d + 1))]
         score += count_sequences(diag2, player)
 
     return score
